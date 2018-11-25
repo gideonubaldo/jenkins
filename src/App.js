4221a643b210b725e362e4f82a99977eeb1f8e86
Oobnala <alnboo1@gmail.com>
2018-11-24 21:53:43 -0800

    Connect our app to Okta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import Navbar from './navbar/Navbar';
+import HomePage from './home/HomePage';
+import Login from './auth/Login';
+import WebPortal from './webportal/WebPortal'
+
+const config = {
+  issuer: 'https://dev-500578.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oahrdfs7sxBGFkES0h7'
+}
+
 
 class App extends Component {
+  onAuthRequired = ({history}) => {
+    history.push('/login');
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+                  onAuthRequired={this.onAuthRequired}
+        >
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+          </Switch>
+          <Route 
+            path="/(.+)"
+            render={() => (
+              <div>
+                <Switch>
+                  <Route path="/login" render={() => <Login baseUrl='https://dev-500578.oktapreview.com' />}/>
+                  <Route path='/implicit/callback' component={ImplicitCallback}/>
+                  <Route path='/portal' component={WebPortal}/>
+                </Switch>
+              </div>
+            )}/>  
+        </Security>
+
       </div>
+
     );
   }
 }
