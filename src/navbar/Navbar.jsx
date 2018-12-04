3d9ff78c5c69d988255dab0b980b734f39dd99c8
Gideon <gideon.ubaldo@sjsu.edu>
2018-12-03 17:58:10 -0800

    fixed login button

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { withStyles } from "@material-ui/core/styles";
+import { withAuth } from "@okta/okta-react";
 
 const styles = {
   root: {
@@ -20,30 +21,77 @@ const styles = {
   }
 };
 
-class NavBar extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-              Tabs
-            </Typography>
-            <Button href="/" color="inherit">Home</Button>
-            <Button href="/portal" color="inherit">Settings</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
+export default withStyles(styles)(
+  withAuth(
+    class NavBar extends Component {
+      state = { authenticated: null };
 
-export default withStyles(styles)(NavBar);
+      checkAuthentication = async () => {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+          this.setState({ authenticated });
+        }
+      };
+
+      async componentDidMount() {
+        this.checkAuthentication();
+      }
+
+      async componentDidUpdate() {
+        this.checkAuthentication();
+      }
+
+      login = async () => {
+        this.props.auth.login("/");
+      };
+
+      logout = async () => {
+        this.props.auth.logout("/");
+      };
+
+      render() {
+        if (this.state.authenticated === null) return null;
+        const logInOut = this.state.authenticated ? (
+          <div>
+            <Button color="primary" variant="contained" onClick={this.logout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button color="primary" variant="contained" onClick={this.login}>
+              Login
+            </Button>
+          </div>
+        );
+        const { classes } = this.props;
+        return (
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="Menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  className={classes.grow}
+                >
+                  Tabs
+                </Typography>
+                <Button href="/" color="inherit">
+                  Home
+                </Button>
+                {logInOut}
+              </Toolbar>
+            </AppBar>
+          </div>
+        );
+      }
+    }
+  )
+);
